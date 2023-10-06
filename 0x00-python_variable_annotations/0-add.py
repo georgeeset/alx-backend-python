9dd652517afc14d815372c44696a0f1956376d8a
georgeeset <georgeperfect4u@gmail.com>
2023-10-06 10:43:31 +0100

    add float and concatinate string using anotated functions

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,11 @@
+"""
+Write a type-anotated function that takes a float a and float b
+as arguments and returns their sum as float
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    Add two float values
+    """
+    return a + b
---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,11 @@
+"""
+a type-annotated function concat that takes a string str1 and a string
+str2 as arguments and returns a concatenated string
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    concatinate two strings
+    """
+    return str1 + str2
