94103f9c778876d7f44b000ea107dcd26a75b5ed
georgeeset <georgeperfect4u@gmail.com>
2023-10-06 12:43:52 +0100

    complec types- functions

---
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+A type-annotated function make_multiplier that takes a float
+multiplier as argument and returns a function that multiplies
+a float by multiplier.
+"""
+
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """
+    Takes a float and returns a function9str) -> fleat (callable)
+    """
+    return (lambda x: x*multiplier)
