7863692d0c3c3e8acb82690f85322b17f0cee6b9
georgeeset <georgeperfect4u@gmail.com>
2023-10-09 15:37:44 +0100

    The basics of async
    Lets execute multiple coroutines at the same time with async

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -11,6 +11,6 @@ async def wait_random(max_delay: int = 10) -> float:
     Function that delays at random seconds within the range of
     max_delay argument.
     """
-    delay: float = random.uniform(0, max_delay)
+    delay: float = random(0, max_delay)
     await asyncio.sleep(delay)
     return delay
