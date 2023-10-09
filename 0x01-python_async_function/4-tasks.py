54b115647ebaabe54490f976762b91262a1c3021
georgeeset <georgeperfect4u@gmail.com>
2023-10-09 19:56:47 +0100

    cant belive i could forget to add the header file before pushing severally

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 """
 Alter task 1 wait_n fnction to call task_wait_random
 """
@@ -8,7 +9,7 @@ task_wait_random = __import__('3-tasks').task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
-    '''takes in an argument, waits for a random delay, returns it.'''
+    """call an async function and return the esult from the function"""
     my_tasks = [task_wait_random(max_delay) for _ in range(n)]
     delay = [await task for task in asyncio.as_completed(my_tasks)]
     return delay
